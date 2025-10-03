197bba95eaae2b75273b5bfb567cbe756037053c
Tuan Nguyen <tuan.beplus@gmail.com>
2025-10-03 16:28:01 +0700

    Update ACF options and Hook to Yoast SEO data

--- includes/functions.php
+++ includes/functions.php
@@ -17,8 +17,19 @@ add_action( 'init', function() {
     remove_post_type_support('page', 'editor');
 }, 99);
 
+$website_url = get_field('website_url', 'option') ?: "https://signlab.com.au";
 
-// echo "<pre>";
-// print_r(get_field('header_logo', 'option'));
-// print_r(get_field('main_menu', 'option'));
-// echo "</pre>";
+add_filter('wpseo_canonical', function($canonical) use ($website_url) {
+    return str_replace(home_url(), $website_url, $canonical);
+});
+
+add_filter('wpseo_opengraph_url', function($url) use ($website_url) {
+    return str_replace(home_url(), $website_url, $url);
+});
+
+add_filter('wpseo_json_ld_output', function($data, $context) use ($website_url) {
+    $search  = home_url();
+    $replace = $website_url;
+
+    return json_decode(str_replace($search, $replace, wp_json_encode($data)), true);
+}, 10, 2);
